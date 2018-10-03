d36b5364369bb9bed883978b5ba0253e465e5441
juniorschmitz <juniorschmitz9@gmail.com>
2018-10-03 01:59:58 -0300

    improvement: leave help features from loading

--- automation-ui/features/pages/home_page.rb
+++ automation-ui/features/pages/home_page.rb
@@ -6,11 +6,4 @@ class HomePage < SitePrism::Page
   set_url 'http://automationpractice.com/'
   section  :header, Desktop::Header, '#header'
   element  :input_search, '#search_query_top'
-
-  
-  def load
-    super
-    highlight(input_search)
-    set_char_by_char(input_search, 'aaaaaaa')
-  end
 end
